ad7832bbd2a0cf9009d5a995297ff266afeefb9d
Jead Fuentes <jfuentes01234@gmail.com>
2024-04-04 11:00:46 +0800

    fix commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 import './bootstrap';
 import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js';
+import 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js';
 
 import Alpine from 'alpinejs';
 
