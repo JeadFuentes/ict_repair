d94b82773a43a556a5ef98dc283cff192f453e74
Jead Fuentes <jfuentes01234@gmail.com>
2024-04-24 16:59:00 +0800

    commiting all files

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -16,23 +16,13 @@
                             <div class="col-sm-4"><h2>Employee <b>Details</b></h2></div>
                             <div class="col-sm-2"><h2>Select <b>Status</b></h2></div>
                             <div class="col-sm-6" style="display:flex;">
-                                <div class="form-check" style="margin-left: 20px">
-                                    <input class="form-check-input" type="radio" name="chkstatus" id="chkstatus" value="Ongoing" hidden>
-                                    <label class="form-check-label" for="chkstatus">
-                                    Ongoing
-                                    </label>
-                                </div>
-                                <div class="form-check" style="margin-left: 20px">
-                                    <input class="form-check-input" type="radio" name="chkstatus" id="chkstatus1" value="Onhold"  hidden>
-                                    <label class="form-check-label" for="chkstatus1">
-                                    Onhold
-                                    </label>
-                                </div>
-                                <div class="form-check" style="margin-left: 20px">
-                                    <input class="form-check-input" type="radio" name="chkstatus" id="chkstatus2" value="Closed" hidden>
-                                    <label class="form-check-label" for="chkstatus2">
-                                    Closed
-                                    </label>
+                                <div class="form-group">
+                                    <select class="form-control" name="division" id="form-division">
+                                    <option>All</option>
+                                    <option>Ongoing</option>
+                                    <option>Onhold</option>
+                                    <option>Closed</option>
+                                    </select>
                                 </div>
                             </div>
                         </div>
@@ -55,6 +45,14 @@
                         </tr>
                     </thead>
                     <tbody>
+                        @php
+                            if (division = "All"){
+                                $Repairtickets = repairticket::query()->orderby('created_at', 'desc')->where('status', All);
+                            }
+                            elseif (division = "Ongoing") {
+                                $Repairtickets = repairticket::query()->orderby('created_at', 'desc')->where('status', Ongoing);
+                            }
+                        @endphp
                         @foreach ($repairtickets as $repairticket)
                         <tr>
                             <th class = "id" scope="row">{{$repairticket->id}}</th>
@@ -69,13 +67,14 @@
                             <td class = "addtlstatus" hidden></td>
                             <td class = "status">{{$repairticket->status}}</td>
                             <td>
-                                
+                                <div class="float-right">
                                 <a href="{{ Route('repairticket.edit',['repairticket' => $repairticket]) }}" class="edit"><p class="btn btn-primary btn-sm">Edit</p></a>
                                 <form action="{{ Route('repairticket.delete',['repairticket' => $repairticket]) }}" method="POST" enctype = "multipart/form-data" class="float-right">
                                     @csrf
                                     @method('delete')
                                     <button> <a class="delete"><p class="btn btn-danger btn-sm">Delete</p></a></button>
                                 </form>
+                            </div>
                             </td>
                             </tr>
                         @endforeach
